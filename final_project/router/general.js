c148543fc3c431f1cd5147d50f7a1b1e4377f3f2
Muhammad Moinudddin <moinuddin010203@gmail.com>
2025-06-19 02:32:35 +0500

    Task 03

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -35,7 +35,28 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const author = req.params.author;
+  let result = [];
+
+  for (let key in books) {
+    if (books[key].author === author) {
+      result.push({
+        id: key,
+        AuthorName: books[key].author,
+        title: books[key].title,
+        reviews: books[key].reviews,
+      });
+    }
+  }
+
+  if (result.length > 0) {
+    return res.status(200).json({ BookAuthor: result });
+  } else {
+    return res.status(404).json("Not Found");
+  }
+
+  // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
