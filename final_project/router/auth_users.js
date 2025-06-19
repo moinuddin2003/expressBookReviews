471a756214bf0527ab0ca068a60d072e6be8928f
Muhammad Moinudddin <moinuddin010203@gmail.com>
2025-06-19 17:05:07 +0500

    Task 09

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -91,6 +91,26 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
 
   // return res.status(300).json({ message: "Yet to be implemented" });
 });
+
+regd_users.delete("/auth/review/:isbn" , (req,res) =>
+{
+  const username = req.session.authorization.username;
+  const isbn = req.params.isbn;
+  // const review = req.query.review;
+
+  if(books[isbn].reviews[username])
+  {
+    delete books[isbn].reviews[username]
+    return res.status(200).json({message: 'Review Deleted Successfully'})
+  }
+  else
+  {
+    return res.status(404).json({message:'Review Not Found'})
+  }
+});
+
+
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
